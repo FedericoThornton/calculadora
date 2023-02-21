672fb5a5aeb5a6bbe24a1ea339d4601f62c906c1
federico thornton <you@example.com>
2023-02-21 20:16:57 -0300

    primera subida

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+             // paso 0: 
+             int Tf, resultado;
+            
+
+             
+
+             // paso 1 pedir valores
+             Console.WriteLine("Ingrese el total facturado: ");
+             Tf = int.Parse(Console.ReadLine());
+             
+                      
+             // paso 2: realizar calculo
+              resultado = (Tf 5%) + 15000;
+
+             // paso 3: emitir resultado en pantalla
+              Console.WriteLine("El sueldo total es: " + resultado);
+        
+
+
+
+        }
+    }
+}
